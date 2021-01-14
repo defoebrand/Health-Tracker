153f4e80a97c04dc37ee949cb75b7322639a6143
defoebrand <defoe.brand@gmail.com>
2021-01-14 17:00:44 +0200

    Set up homepage Carousel and Footer

--- app/javascript/containers/App.js
+++ app/javascript/containers/App.js
@@ -1,12 +1,15 @@
 import React from 'react'
 import HeaderNav from '../components/HeaderNav'
 import SignIn from '../components/SignIn'
+import HomePage from '../components/HomePage'
+import Footer from '../components/Footer'
 
 const App = () => (
   <div className="App">
     <HeaderNav/>
     <HomePage/>
     {/*<SignIn />*/}
+    <Footer/>
   </div>
 );
 

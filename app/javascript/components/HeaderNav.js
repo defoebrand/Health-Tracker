6794c7d6ee505c3eacc3e113a0cbdebb9a5da832
defoebrand <defoe.brand@gmail.com>
2021-01-14 21:56:26 +0200

    Set history to push home

--- app/javascript/components/HeaderNav.js
+++ app/javascript/components/HeaderNav.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 // import Image from 'react-bootstrap/Image';
@@ -7,32 +7,40 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import Form from 'react-bootstrap/Form';
 import FormControl from 'react-bootstrap/FormControl';
 
-const HeaderNav = () => (
-  <div className="HeaderNav">
-    <Navbar bg="light" expand="lg">
-      <Navbar.Brand href="#home">
-        <h1>Hello</h1>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="basic-navbar-nav" />
-      <Navbar.Collapse id="basic-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav.Link href="#home">Home</Nav.Link>
-          <Nav.Link href="./SignIn">Learning</Nav.Link>
-          <NavDropdown title="Community" id="basic-nav-dropdown">
-            <NavDropdown.Item href="#action/3.1">Service</NavDropdown.Item>
-            <NavDropdown.Item href="#action/3.2">Charity</NavDropdown.Item>
-            {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-            <NavDropdown.Divider />
-            <NavDropdown.Item href="#action/3.4">Sign in / Register</NavDropdown.Item>
-          </NavDropdown>
-        </Nav>
-        <Form inline>
-          <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-          <Button variant="outline-success">Search</Button>
-        </Form>
-      </Navbar.Collapse>
-    </Navbar>
-  </div>
-);
+import { useHistory } from 'react-router-dom';
+
+const HeaderNav = () => {
+  const history = useHistory();
+  useEffect(() => {
+    history.push('/home');
+  }, []);
+  return (
+    <div className="HeaderNav">
+      <Navbar bg="light" expand="lg">
+        <Navbar.Brand href="#home">
+          <h1>Hello</h1>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+          <Nav className="mr-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="./SignIn">Learning</Nav.Link>
+            <NavDropdown title="Community" id="basic-nav-dropdown">
+              <NavDropdown.Item href="#action/3.1">Service</NavDropdown.Item>
+              <NavDropdown.Item href="#action/3.2">Charity</NavDropdown.Item>
+              {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
+              <NavDropdown.Divider />
+              <NavDropdown.Item href="#action/3.4">Sign in / Register</NavDropdown.Item>
+            </NavDropdown>
+          </Nav>
+          <Form inline>
+            <FormControl type="text" placeholder="Search" className="mr-sm-2" />
+            <Button variant="outline-success">Search</Button>
+          </Form>
+        </Navbar.Collapse>
+      </Navbar>
+    </div>
+  );
+};
 
 export default HeaderNav;

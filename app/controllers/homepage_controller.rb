383ebfbf695eaa0b6d63ad02c6e8183739e2a035
defoebrand <defoe.brand@gmail.com>
2021-01-20 16:04:46 +0200

    Any match of any search parameter returned in api call

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,36 +1,65 @@
 class HomepageController < ApplicationController
   def index; end
 
-  def allusers
+  def api
+    if params.keys.count <= 2
+      return_all_users
+    else
+      return_query
+    end
+  end
+
+  private
+
+  def return_all_users
+    @users = User.all
+    @return_user = sanitize_user(@users)
+    render json: @return_user
+  end
+
+  def return_query
     @users = []
     params.each do |param|
-      @user = User.find_by(param[0] => param[1]) unless %w[action controller].include?(param[0])
+      next if %w[action controller].include?(param[0])
+
+      User.where("#{param[0]} = ?", param[1]).find_each do |user|
+        @users << user
+      end
     end
-    # @users = User.all
-    render json: {
-      id: @user.id,
-      age: @user.age,
-      height: @user.height,
-      weight: @user.weight,
-      systolic: JSON.parse(@user.systolic),
-      diastolic: JSON.parse(@user.systolic),
-      pulse: JSON.parse(@user.systolic),
-      temperature: JSON.parse(@user.systolic),
-      blood_sugar: JSON.parse(@user.systolic)
-    }
-  end
+    @return_user = sanitize_user(@users)
+    if !@return_user.count.zero?
+      @unique_users = only_unique(@return_user)
+      render json: @unique_users
 
-  def user
-    @user = User.find(params[:id])
-    render json: @user
-  end
-
-  def search_by_age
-    @user = User.find_by(age: params[:age])
-    if @user
-      render json: @user
     else
       render json: { message: 'No matching result' }
     end
+  end
+
+  def sanitize_user(user_array)
+    @return_user = []
+    user_array.each do |user|
+      @user = {
+        id: user.id,
+        age: user.age,
+        height: user.height,
+        weight: user.weight,
+        systolic: JSON.parse(user.systolic || '{}'),
+        diastolic: JSON.parse(user.systolic || '{}'),
+        pulse: JSON.parse(user.systolic || '{}'),
+        temperature: JSON.parse(user.systolic || '{}'),
+        blood_sugar: JSON.parse(user.systolic || '{}')
+      }
+      @return_user << @user
+    end
+    @return_user
+  end
+
+  def only_unique(users_to_return)
+    return_users = users_to_return.uniq do |user|
+      puts user['id']
+      user['id']
+    end
+    return_users
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,36 +1,15 @@
 Rails.application.routes.draw do
   root 'homepage#index'
-
-  # resources :homepage, module: :api do
-  #   resources :homepage, only: %i[users]
-  # end
-  # namespace :api do
-  scope path: '/api' do
-    get '/', to: 'homepage#allusers'
-  end
-  scope path: '/api/id' do
-    get '/:id', to: 'homepage#user'
-  end
-  scope path: '/api/age' do
-    get '/:age', to: 'homepage#search_by_age'
-  end
   
-  # resources :homepage, module: 'api'
-  
-  # scope path: "/api", as: 'admin' do
-    # resources :homepage, only: %i[users]
-  # end
-
-
-
-
-
   resources :user do
     collection do
       post 'login'
     end
   end
 
-  # get 'homepage/SignIn'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  scope path: '/api' do
+    get '/', to: 'homepage#api'
+  end
+
+
 end

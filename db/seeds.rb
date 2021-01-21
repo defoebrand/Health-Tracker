b9002679851dc75d37a86349a26d43cd83ed7ef6
defoebrand <defoe.brand@gmail.com>
2021-01-21 09:04:08 +0200

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -19,3 +19,44 @@ User.create(
   temperature: 37,
   blood_sugar: 5.03
 )
+
+Community.create(
+  name: 'Diabetes',
+  image: '/images/diabetes-ribbon.jpeg',
+  description: 'A group of metabolic disorders characterized by a high blood sugar level over a prolonged period of time',
+)
+Community.create(
+  name: 'Pregnancy',
+  image: '/images/pregnancy-ribbon.jpeg',
+  description: 'The time during which one or more offspring develops inside a woman.',
+)
+Community.create(
+  name: 'Cancer',
+  image: '/images/cancer-ribbons.jpg',
+  description: 'A group of diseases involving abnormal cell growth with the potential to invade or spread to other parts of the body.',
+)
+Community.create(
+  name: 'Anemia',
+  image: '/images/anemia-ribbon.jpeg',
+  description: 'A decrease in the total amount of red blood cells (RBCs) or hemoglobin in the blood, or a lowered ability of the blood to carry oxygen.',
+)
+Community.create(
+  name: 'Autism',
+  image: '/images/autism-ribbon.jpeg',
+  description: 'A developmental disorder characterized by difficulties with social interaction and communication, and by restricted and repetitive behavior.',
+)
+Community.create(
+  name: "Chron's - Colitis",
+  image: '/images/chrons-colitis-ribbon.png',
+  description: "Crohn's disease is a type of inflammatory bowel disease (IBD) that may affect any segment of the gastrointestinal tract from the mouth to the anus. Colitis is an inflammation of the colon that may be acute and self-limited or long-term.",
+)
+Community.create(
+  name: 'Autoimmune',
+  image: '/images/autoimmune-ribbon.jpeg',
+  description: 'A condition arising from an abnormal immune response to a functioning body part. There are at least 80 types of autoimmune diseases.',
+)
+Community.create(
+  name: 'Migraine',
+  image: '/images/migraine-ribbon.png',
+  description: 'A primary headache disorder characterized by recurrent headaches that are moderate to severe.',
+)

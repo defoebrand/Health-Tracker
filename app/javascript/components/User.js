122cb74f69b518c497f7b605ab09080dd4fed157
defoebrand <defoe.brand@gmail.com>
2021-01-21 14:11:26 +0200

    Add base line stats to charts

--- app/javascript/components/User.js
+++ app/javascript/components/User.js
@@ -43,6 +43,81 @@ const User = () => {
         setDiastolic(JSON.parse(localDiastolic));
       }).catch(err => console.log(err));
   }, [history]);
+  const basePulse = user => {
+    if (user.age > 65) {
+      return 80;
+    } if (user.age > 20) {
+      return 60;
+    } if (user.age > 12) {
+      return 80;
+    } if (user.age > 5) {
+      return 100;
+    } if (user.age > 2) {
+      return 115;
+    } if (user.age > 1) {
+      return 125;
+    }
+    return 135;
+  };
+  const baseTemp = user => {
+    if (user.age > 10 && user.age < 65) {
+      return 37;
+    }
+    return 36;
+  };
+  const baseSys = user => {
+    if (user.age > 59) {
+      return 134;
+    } if (user.age > 54) {
+      return 131;
+    } if (user.age > 49) {
+      return 129;
+    } if (user.age > 44) {
+      return 127;
+    } if (user.age > 39) {
+      return 125;
+    } if (user.age > 34) {
+      return 123;
+    } if (user.age > 29) {
+      return 122;
+    } if (user.age > 24) {
+      return 121;
+    } if (user.age > 19) {
+      return 120;
+    }
+    return 117;
+  };
+
+  const baseDia = user => {
+    if (user.age > 59) {
+      return 87;
+    } if (user.age > 54) {
+      return 86;
+    } if (user.age > 49) {
+      return 85;
+    } if (user.age > 44) {
+      return 84;
+    } if (user.age > 39) {
+      return 83;
+    } if (user.age > 34) {
+      return 82;
+    } if (user.age > 29) {
+      return 81;
+    } if (user.age > 24) {
+      return 80;
+    } if (user.age > 19) {
+      return 79;
+    }
+    return 77;
+  };
+  const baseBloodSugar = user => {
+    if (user.age > 19) {
+      return 5.0;
+    } if (user.age > 12) {
+      return 6.1;
+    }
+    return 7.2;
+  };
   const newPulseData = [];
   const newTempData = [];
   const newBloodSugarData = [];
@@ -55,30 +130,34 @@ const User = () => {
   // console.log('user', user);
   pulseData.forEach(key => {
     Object.entries(pulse[key]).forEach(item => {
-      newPulseData.push({ name: key, [`${user.name} Pulse`]: item[1] });
+      newPulseData.push({ name: key, [`${user.name} Pulse`]: item[1], basePulse: basePulse(user) });
     });
   });
   tempData.forEach(key => {
     Object.entries(temp[key]).forEach(item => {
-      newTempData.push({ name: key, [`${user.name} Temp`]: item[1] });
+      newTempData.push({ name: key, [`${user.name} Temp`]: item[1], baseTemp: baseTemp(user) });
     });
   });
   bloodSugarData.forEach(key => {
     Object.entries(bloodSugar[key]).forEach(item => {
-      newBloodSugarData.push({ name: key, [`${user.name} Blood Sugar`]: item[1] });
+      newBloodSugarData.push({ name: key, [`${user.name} Blood Sugar`]: item[1], baseBloodSugar: baseBloodSugar(user) });
     });
   });
   if (systolicData.length !== 0) {
     systolicData.forEach(key => {
       Object.entries(systolic[key]).forEach(item => {
-        newBloodPressureData.push({ name: key, systolic: item[1] });
+        newBloodPressureData.push({ name: key, systolic: item[1], baseSys: baseSys(user) });
       });
     });
     newBloodPressureData.forEach((entry, index) => {
       diastolicData.forEach(key => {
         Object.entries(diastolic[key]).forEach(item => {
           if (key === newBloodPressureData[index].name) {
-            newBloodPressureData[index] = { ...newBloodPressureData[index], diastolic: item[1] };
+            newBloodPressureData[index] = {
+              ...newBloodPressureData[index],
+              diastolic: item[1],
+              baseDia: baseDia(user),
+            };
           }
         });
       });

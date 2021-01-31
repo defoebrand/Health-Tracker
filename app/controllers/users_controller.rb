fb3e2644d3f2a57d379b00608416d8c851426f37
defoebrand <defoe.brand@gmail.com>
2021-01-31 15:14:35 +0200

    Use created user instead of second db query

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -29,7 +29,7 @@ class UsersController < ApplicationController
 
     if @user.valid?
       token = encode_token({ user_id: @user.id })
-      render json: { user: cleanse_user(User.find(@user.id)), token: token }
+      render json: { user: cleanse_user(@user), token: token }
     else
       render json: { error: 'Incorrect Input Supplied' }
     end
